33c0be64355b5e1494a6fb1b52d90be4c77d4d89
zcomert <zcomert@samsun.edu.tr>
2025-10-07 10:54:20 +0300

    Updated.

--- Controllers/MbController.cs
+++ Controllers/MbController.cs
@@ -8,4 +8,10 @@ public class MbController : Controller
     {
         return View();
     }
+
+    public IActionResult Introduce()
+    {
+        var name = "Zafer CÖMERT";
+        return View("Introduce", name);
+    }
 }
