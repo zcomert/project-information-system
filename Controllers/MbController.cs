2e6ffee4c51fa168cf482cef966c7025ce7586a8
zcomert <zcomert@samsun.edu.tr>
2025-10-07 11:44:45 +0300

    ModelBinding has been discovered.

--- Controllers/MbController.cs
+++ Controllers/MbController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using PIS.Models;
 
 namespace PIS.Controllers;
 
@@ -14,4 +15,30 @@ public class MbController : Controller
         var name = "Zafer CÖMERT";
         return View("Introduce", name);
     }
+    public IActionResult Person()
+    {
+        var person = new Person()
+        {
+            Id = 1,
+            FirstName = "Görkem",
+            LastName = "Ediz Özpekmezci",
+            Salary = 100_000_000
+        };
+        return View(person);
+    }
+    public IActionResult Cities()
+    {
+        var cities = new List<string>()
+        {
+            "Samsun",
+            "Sinop",
+            "Şanlıurfa",
+            "Ankara",
+            "Tokat",
+            "Çorum",
+            "Hatay"
+        };
+        return View(cities);
+    }
+
 }
---
+++ Models/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace PIS.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public decimal Salary { get; set; }
+    }
+}
