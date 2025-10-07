18bcd6cf062daf2a22453604b7595bc02f62d5a8
zcomert <zcomert@samsun.edu.tr>
2025-10-07 13:59:25 +0300

    version_2 added

---
+++ Controllers/MbController_v2.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PIS.Controllers
+{
+    public class MbController_v2 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
