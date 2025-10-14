c04b00978311195e0742499c42b9ea153cc84a01
zcomert <zcomert@samsun.edu.tr>
2025-10-14 15:49:24 +0300

    updated

---
+++ Controllers/ProjectManagerController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PIS.Models;
+
+namespace PIS.Controllers
+{
+    public class ProjectManagerController : Controller
+    {
+        public IActionResult Index()
+        {
+            var manager = new ProjectManager();
+            var list1 = manager.GetAll();
+            return View(list1);
+        }
+    }
+}
