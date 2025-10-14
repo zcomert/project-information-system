9a59ced462b20653d86aab415ab583eeb4aee981
zcomert <zcomert@samsun.edu.tr>
2025-10-14 12:17:15 +0300

    ProjectManager has been defined.

--- Controllers/ProjectManagerController.cs
+++ Controllers/ProjectManagerController.cs
@@ -11,4 +11,20 @@ public class ProjectManagerController : Controller
         var list = manager.GetAll();
         return View(list);
     }
+
+    public IActionResult Details([FromRoute(Name ="id")] int id)
+    {
+        var manager = new ProjectManager();
+        var list = manager.GetAll();
+        // var project = new Project();
+        // foreach (Project item in list)
+        // {
+        //     if (item.Id == id)
+        //     {
+        //         project = item;
+        //         break;
+        //     }
+        // } 
+        return View(list.SingleOrDefault(p => p.Id == id));
+    }
 }
