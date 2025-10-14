44b46c2b93cce0ae417c7fb78153faac4b8fd1ae
zcomert <zcomert@samsun.edu.tr>
2025-10-14 14:28:48 +0300

    ProjectManager has been updated.

---
+++ Models/proje.cs
@@ -0,0 +1,9 @@
+﻿namespace PIS.Models
+{
+    public class Project
+    {
+        public Int16 Id { get; set; }
+        public String Title { get; set; }
+        public Decimal Budget { get; set; }
+    }
+}
