6c8b3270d536179ad601556118cc688154f7e00f
zcomert <zafer.comert@hotmail.com>
2025-12-23 09:00:15 +0300

    Updated.

--- Program.cs
+++ Program.cs
@@ -39,7 +39,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "areas",
-    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+    pattern: "{area:exists}/{controller=Dashboard}/{action=Index}/{id?}"
 );
 
 app.MapControllerRoute(
